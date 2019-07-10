c1f9f8ff8ac83cfb43dfbd9a650673fcf25123c5
tom <tom.meyer89@gmail.com>
2019-07-10 17:16:54 +0200

    added write endpoint dummy and show+database cmds

--- scripts/influx.py
+++ scripts/influx.py
@@ -5,22 +5,41 @@ from json import dumps
 # influxql https://docs.influxdata.com/influxdb/v1.7/query_language/data_exploration/
 # influxcli https://docs.influxdata.com/influxdb/v1.7/tools/shell/
 
+# https://docs.influxdata.com/influxdb/v1.7/query_language/spec/#dates-times
+timeformat = "%Y-%m-%d %H:%M:%S"  # plus microseconds -> seems to be isoformat
+
 
 def pretty(json):
     return dumps(json, indent=1)
 
 
-class influx:
-    connected = False
+class Influx:
     host = None
     port = None
+    database = None
+    show = None
 
     def __init__(self, host="127.0.0.1", port=8086):
+        self._construct(host, port)
+        self.check_connection()
+        self.database = _DataBase(host, port)
+        self.show = _Show(host, port)
+
+    # break recursion
+    def _construct(self, host, port):
         self.host = host
         self.port = port
-        self.check_connection
 
-    def query(self, query, db=None):
+    def check_connection(self):
+        request = str.format("http://{}:{}/ping", self.host, self.port)
+        try:
+            requests.get(request)
+        except Exception as e:
+            error("Unable to connect to host: " + str(e))
+            return False
+        return True
+
+    def _query(self, query, db=None):
         request = str.format("http://{}:{}/query?", self.host, self.port)
         if db != None:
             request = str.format("{}db={}", request, db)
@@ -32,25 +51,17 @@ class influx:
         debug(pretty(r.json()))
         return r.json()
 
-    def check_connection(self):
-        try:
-            self.query('show databases')
-        except Exception as e:
-            error("Unable to connect to host: " + str(e))
-            return False
-        return True
-
-    def show_databases(self):
-        return self.query("show databases")
-
-    def show_queries(self):
-        return self.query("show queries")
-
-    def show_meassurements(self):
-        return self.query("show measurements")
-
-    def show_diagnostics(self):
-        return self.query("show diagnostics")
+    def _write(self, data, db=None):
+        request = str.format("http://{}:{}/write?", self.host, self.port)
+        if db != None:
+            request = str.format("{}db={}", request, db)
+        r = requests.post(request, {'q': data})
+        debug(r.url)
+        if not r.ok:
+            error(r.text)
+            r.raise_for_status()
+        debug(pretty(r.json()))
+        return r.json()
 
     def select(self, database, keys=[], tags=[], measurements=[], condition=""):
         """
@@ -79,16 +90,41 @@ class influx:
         cmd = "SELECT " + select_clause + " FROM " + ', '.join(measurements)
         if condition != "":
             cmd = cmd + " WHERE " + condition
-        self.query(cmd, database)
+        self._query(cmd, database)
 
 
-db = influx('192.168.1.2', 8086)
-# db.host='192.168.1.2'
-# db.port=8086
-# db.query("")
-# db.check_connection()
-#db.select("test", measurements=["test"])
-print(pretty(db.show_databases()))
-print(pretty(db.show_diagnostics()))
-print(pretty(db.show_meassurements()))
-print(pretty(db.show_queries()))
+class _DataBase(Influx):
+    def __init__(self, host, port):
+        self._construct(host, port)
+
+    def create(self, name):
+        return self._query(str.format('create database "{}"', name))
+
+    def drop(self, name):
+        return self._query(str.format('drop database "{}"', name))
+
+
+class _Show(Influx):
+    def __init__(self, host, port):
+        self._construct(host, port)
+
+    def databases(self):
+        return self._query("show databases")
+
+    def queries(self):
+        return self._query("show queries")
+
+    def meassurements(self):
+        return self._query("show measurements")
+
+    def diagnostics(self):
+        return self._query("show diagnostics")
+
+
+db = Influx('192.168.1.2', 8086)
+# db.select("test", measurements=["test"])
+# print(pretty(db.show_diagnostics()))
+# print(pretty(db.show_meassurements()))
+# print(pretty(db.show_queries()))
+# print(db.database.drop("asda"))
+print(pretty(db.show.databases()))
